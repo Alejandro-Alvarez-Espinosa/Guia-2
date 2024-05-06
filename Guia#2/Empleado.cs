fca07ad3f80d37ec1a491644d63182aa795e4257
Alejandro Alvarez <alejandro.alvarez17u@std.uni.edu.ni>
2024-05-05 21:04:36 -0600

    Problema V1 y V2

---
+++ Guia#2/Empleado.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_2
+{
+    internal class Empleado
+    {
+        //Variable miembro de la clase empleado 
+        private string Nombre;
+        private int NumeroEmpleado;
+        private decimal Salario;
+        private bool Activo; //Estado del empleado (Activo o Inactivo)
+
+
+        public Empleado(string nombre, int numeroEmpleado, decimal salario, bool activo = true)
+        {
+            this.Nombre = nombre;
+            this.NumeroEmpleado = numeroEmpleado;
+            this.Salario = salario;
+            this.Activo = activo;
+        }
+
+        //Metodo para mostrar los detalles del empleado
+        public void MostrarDetalles()
+        {
+            string estado = (Activo) ? "Activo" : "Inactivo";
+            Console.WriteLine($"Nombre: {Nombre}, Número de Empleado: {NumeroEmpleado}, Salario: {Salario:C}, Estado: {estado}");
+        }
+        //Metodo para obtener el numero de un empleado
+        public int ObtenerNumeroEmpleado()
+        {
+            return this.NumeroEmpleado;
+        
+        }
+
+        //Metodo para cambiar el estado de un empleado
+        public void CambiarEstado(int nuevoEstado) 
+        {
+            if (nuevoEstado == 1)
+            {
+                Activo = true; //Activar Empleado
+                Console.WriteLine("Empleado activado");
+            }
+            else if (nuevoEstado == 0)
+            {
+                Activo = false; //Desactivar Empleado
+                Console.WriteLine("Empleado desactivado");
+            }
+            else
+            {
+                Console.WriteLine("Estado no valido ");
+
+            }
+        }
+
+        //Método para aumentar el salario en un porcentaje determinado
+        public void AumentarSalario(decimal porcentajeAumento)
+        {
+            if (porcentajeAumento <= 0)
+            {
+                throw new Exception("El porcentaje de aumento debe ser un valor positivo");
+            }
+
+            decimal nuevoSalario = Salario + (Salario * porcentajeAumento / 100);
+            Salario = nuevoSalario;
+            Console.WriteLine($"El salario de {Nombre} ha sido aumentado a {Salario:C}");
+        }
+
+    }
+}
---
+++ Guia#2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear una lista para almacenar empleados
+            List<Empleado> listaEmpleado = new List<Empleado>();
+
+            //Agregar empleados de ejemplo a la lista
+            listaEmpleado.Add(new Empleado("Juan", 101, 30000));
+            listaEmpleado.Add(new Empleado("María", 102, 35000));
+            listaEmpleado.Add(new Empleado("Pedro", 103, 32000, false));
+
+            //Mostrar los detalles de cada empleado en la lista
+            foreach (var empleado in listaEmpleado)
+            {
+                empleado.MostrarDetalles();
+            }
+            //Cambiar el estado de un empleado por su numero de empleado 
+            int numeroEmpleadoCambiarEstado = 103;//Numero de empleado a cambiar estado 
+            Empleado empleadoCambiarEstado = BuscarEmpleado(listaEmpleado, numeroEmpleadoCambiarEstado);
+
+
+            if (empleadoCambiarEstado != null)
+            {
+
+                //Cambiar el estado a activo (1) o inactivo (0)
+                empleadoCambiarEstado.CambiarEstado(1); //Cambiar a activo
+                empleadoCambiarEstado.MostrarDetalles(); //Mostrar detalles actualizados 
+
+            }
+            else
+            {
+
+                Console.WriteLine($"Empleado con número {numeroEmpleadoCambiarEstado} no encontrado.");
+            }
+
+            //Aumentar el salario de un empleado por su número de empleado y porcentaje de aumento
+            int numeroEmpleadoAumentarSalario = 103; // Número de empleado a aumentar el salario
+            decimal porcentajeAumento = 15; // Porcentaje de aumento
+
+            Empleado empleadoAumentarSalario = BuscarEmpleado(listaEmpleado, numeroEmpleadoAumentarSalario);
+
+            if (empleadoAumentarSalario != null)
+            {
+                empleadoAumentarSalario.AumentarSalario(porcentajeAumento);
+                empleadoAumentarSalario.MostrarDetalles(); // Mostrar detalles con el nuevo salario
+            }
+            else
+            {
+                Console.WriteLine($"Empleado con número {numeroEmpleadoAumentarSalario} no encontrado.");
+            }
+
+        }
+
+        //Metodo para buscar un empleado por su numero de empleado
+        static Empleado BuscarEmpleado(List<Empleado> empleados, int numeroEmpleado)
+        {
+            foreach (var empleado in empleados)
+            {
+                if (empleado.ObtenerNumeroEmpleado() == numeroEmpleado)
+                {
+                    return empleado;
+                }
+            }
+
+            return null; //Retorna null si no se encuentra ningun empleado con ese numero
+        }
+    }
+}
+
